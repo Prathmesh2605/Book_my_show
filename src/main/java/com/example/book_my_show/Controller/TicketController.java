e818e41cc38b38b153ee48ac898f8c9716d47ac5
Prathmesh <pcshedge@gmail.com>
2023-03-03 22:45:41 +0530

    Minor changes

--- src/main/java/com/example/book_my_show/Controller/TicketController.java
+++ src/main/java/com/example/book_my_show/Controller/TicketController.java
@@ -1,6 +1,8 @@
-package com.example.book_my_show;
+package com.example.book_my_show.Controller;
 
 
+import com.example.book_my_show.EntryDto.TicketEntryDto;
+import com.example.book_my_show.Services.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
